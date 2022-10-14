ea5b8bb7549fc23143a2819b1bd5cd4ea45b2afd
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2022-10-14 00:27:43 -0400

    001 Mad Libs Vacation with sleep timer

---
+++ 001_MadLibs.py
@@ -0,0 +1,47 @@
+import time
+
+
+print("\nWelcome to MadLibs Vacations")
+print("============================\n\n")
+print("") 
+adj1 = input("Enter an adjective : ")
+adj2 = input("Enter an adjective : ")
+noun1 = input("Enter a noun : ")
+noun2 = input("Enter a noun : ")
+plural_noun1 = input("Enter a plural noun : ")
+game1 = input("Enter A name of a game : ")
+plural_noun2 = input("Enter a plural noun : ")
+INGverb1 = input("Enter a verb ending in 'ING' : ")
+INGverb2 = input("Enter a verb ending in 'ING' : ")
+plural_noun3 = input("Enter a plural noun : ")
+INGverb3 = input("Enter a verb ending in 'ING' : ")
+noun3 = input("Enter a noun : ")
+plant1 = input("Enter a name of a plant : ")
+body_part1 = input("Enter a part of the body : ")
+place1 = input("Enter the name of a Place : ")
+INGverb4 = input("Enter a verb ending in 'ING' : ")
+adj3 = input("Enter an adjective : ")
+number1 = input("Enter a number : ")
+plural_noun4 = input("Enter a plural noun : ")
+
+print("\n\nAre you ready for this!!!\n\n")
+time.sleep(3)
+print(f"""
+
+A vacation is when you take a trip to some {adj1} place
+with your {adj2} family. Usually you go to some place
+that is near a/an {noun1} or up on a/an {noun2}.
+A good vacation place is one where you can ride {plural_noun1}
+or play {game1} or go hunting for {plural_noun2}. I like
+to spend my time {INGverb1} or {INGverb2}.
+When parents go on a vacation, they spend their time eating
+three {plural_noun3} a day. and father's play golf, and mothers
+sit around {INGverb3}. Last summer, my little brother
+fell in a/an {noun3} and got poison {plant1} all 
+over his {body_part1}. My family is going to go to the
+{place1}, and I will practice {INGverb4}. Parents
+need vacations more than kids because parents are alway very
+{adj3} and because they have to work {number1} 
+hours every day all year making enough {plural_noun4} to 
+pay for vacation
+""")
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # PythonMiniProjects2022A
 
 https://www.freecodecamp.org/news/python-projects-for-beginners/ 
+
+001 - MadLibs Summer Vacation
+002 - Guess random
